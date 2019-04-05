1c9bbca29507a0f7db2b24232340e584195b5e41
e <Alex_up_up@163.com>
2019-04-05 09:29:27 +0800

    git init

---
+++ Algorithm/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @Author Alex
+ * @Desc <p></p>
+ * @Date 2019/4/5 9:22
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
